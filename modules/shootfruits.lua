d24549aaa1bebb0c183277f3b0505d54e9d1b27a
Adrian L Lange <git@p3lim.net>
2023-05-02 19:00:40 +0200

    Add automation for shooting fruits in the plains

---
+++ modules/shootfruits.lua
@@ -0,0 +1,25 @@
+local _, addon = ...
+
+local FRUIT_QUEST = 70549 -- Low Hanging Fruit
+local MACRO = ([[
+/target %s
+/click ExtraActionButton1
+]]):format('Honey Plum', (GetSpellInfo(388556)))
+
+local function onQuestRemoved(self, questID)
+	if questID == FRUIT_QUEST then
+		-- reset
+		addon:Unbind()
+		return true
+	end
+end
+
+function addon:QUEST_LOG_UPDATE()
+	if C_QuestLog.IsOnQuest(FRUIT_QUEST) then
+		self:BindMacro(MACRO)
+
+		if not addon:IsEventRegistered('QUEST_REMOVED', onQuestRemoved) then
+			addon:RegisterEvent('QUEST_REMOVED', onQuestRemoved)
+		end
+	end
+end
